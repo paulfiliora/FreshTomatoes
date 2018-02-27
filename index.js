b23eaa6458630400efbb5f5e70bcf7a6b4b531df
Paul <paul.filiora@gmail.com>
2018-02-27 14:11:36 -0500

    Handle Missing Routes. Connect Sequalize. Declare Schema

--- index.js
+++ index.js
@@ -1,22 +1,103 @@
 const sqlite = require('sqlite'),
-      Sequelize = require('sequelize'),
-      request = require('request'),
-      express = require('express'),
-      app = express();
+    Sequelize = require('sequelize'),
+    request = require('request'),
+    express = require('express'),
+    app = express();
 
-const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+const {
+    PORT = 3000, NODE_ENV = 'development', DB_PATH = './db/database.db'
+} = process.env;
 
 // START SERVER
 Promise.resolve()
-  .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
-  .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
+    .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
+    .catch((err) => {
+        if (NODE_ENV === 'development') console.error(err.stack);
+    });
+
+
+// DECLARE SEQUALIZE CONNECTION
+const sequelize = new Sequelize('mainDB', null, null, {
+    host: 'localhost',
+    dialect: 'sqlite',
+    storage: './db/database.db',
+});
+
+// DELARE DB SCHEMA & MODELS
+const Genre = sequelize.define('genre', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+})
+const Film = sequelize.define('film', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    release_date: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    tagline: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    revenue: {
+        type: Sequelize.BIGINT,
+        defaultValue: 0,
+        allowNull: false
+    },
+    budget: {
+        type: Sequelize.BIGINT,
+        allowNull: false
+    },
+    runtime: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    original_language: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    genre_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: Genre,
+            key: 'id'
+        }
+    }
+}, {
+    timestamps: false
+})
 
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
+// MISSING ROUTES
+app.get('*', function(req, res) {
+    res.status(404).send({
+        message: '404 page not found'
+    });
+})
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
+    res.status(500).send('Not Implemented');
 }
 
 module.exports = app;
